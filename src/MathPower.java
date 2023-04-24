9666c3c3e1fc636cb206acaa9a3dec40eb6a4df1
akashb21 <patelakash212121@gmail.com>
2023-04-23 22:03:02 -0400

    Intial version

--- src/MathPower.java
+++ src/MathPower.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
 public class MathPower {
-    public static void main(String []argh){
+    public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
             int calc = a;
-            for(int j=0;j<n;j++)
-            {
-                calc+=(Math.pow(2,j)*b);
-                System.out.print(calc+" ");
+            for (int j = 0; j < n; j++) {
+                calc += (Math.pow(2, j) * b);
+                System.out.print(calc + " ");
             }
-            System.out.println();
         }
         in.close();
     }
