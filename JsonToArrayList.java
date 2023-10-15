6f51b5e662990eefaa1b6ba5389aee77ff749cfa
Akash Patel <43804381+akkipatel21@users.noreply.github.com>
2023-10-15 15:29:36 -0400

    Update JsonToArrayList.java

--- JsonToArrayList.java
+++ JsonToArrayList.java
@@ -4,7 +4,7 @@ class DS {
 
 dw
         List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 4, 5, 6);
-        List<Integer> list2 = Arrays.asList(5, 6, 7, 8);
+        List<Integer> list2 = Arrays.asList(5, 6, 7, 8);d
         // Prepare a union
         Set<Integer> union = new HashSet<Integer>(list1);
         union.addAll(list2);
