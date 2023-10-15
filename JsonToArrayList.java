715a1f419a1306534cb97fae76e8175772ea1c7f
Akash Patel <43804381+akkipatel21@users.noreply.github.com>
2023-10-15 15:30:02 -0400

    Update JsonToArrayList.java

--- JsonToArrayList.java
+++ JsonToArrayList.java
@@ -2,11 +2,11 @@ dwimport java.util.*;
 class DS {
     public static void main(String[] args) {
 
-dw
+
         List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 4, 5, 6);
         List<Integer> list2 = Arrays.asList(5, 6, 7, 8);d
         // Prepare a union
-        Set<Integer> union = new HashSet<Integer>(list1);dw
+        Set<Integer> union = new HashSet<Integer>(list1);
         union.addAll(list2);
         // Prepare an intersection
         Set<Integer> intersection = new HashSet<Integer>(list1);
